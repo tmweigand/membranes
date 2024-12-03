2c765d54dc104eb55d38583e5330b14c14c07792
Andrew Jotcham <ajotcham@Andrews-MacBook-Pro-13.local>
2024-12-03 06:50:34 -0500

    polymerization submit scripts for both 1 and 2

---
+++ domain_generation/pack_poly_2_of_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=64x_2_of_2
+#SBATCH --output=lammps_output.out
+#SBATCH --error=lammps_error.err
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=128
+#SBATCH --time=48:00:00
+#SBATCH --partition=RM
+
+module load openmpi/4.1.1-gcc8.3.1
+mkdir logs
+mkdir restarts_polym
+
+mpirun -np 512 /jet/home/ajotcham/lammps-29-2024/build/lmp -in ../../domain_generation/rec_polymerize_2_of_2.in -var mult 64 -var scale 1.0 -var rand # -var xlink #
