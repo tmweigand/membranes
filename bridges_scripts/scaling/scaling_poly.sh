9cbe4beb96ac002785b9bcbf8fb908c3f0f1ebe3
Andrew Jotcham <ajotcham@andrews-mbp-13.wireless-1x.unc.edu>
2025-01-17 13:19:10 -0500

    scaling submit script change

--- bridges_scripts/scaling/scaling_poly.sh
+++ bridges_scripts/scaling/scaling_poly.sh
@@ -6,6 +6,6 @@ values=(1 2 4 8 16 32)
 # Loop through the values
 for val in "${values[@]}"; do
     echo "Processing value: $val"
-    ./pack_poly_1_of_2.sh "$val"
+    ./pack_poly_1_of_2_nodes_$val.sh
     # Add your commands here that use $val
 done
