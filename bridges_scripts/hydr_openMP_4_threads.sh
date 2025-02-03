7911d06f5fa9dcdb91075ed536adf30a9a20a0f6
Andrew Jotcham <ajotcham@andrews-mbp-13.wireless-1x.unc.edu>
2025-02-03 10:12:51 -0500

    fixed command line arguements

--- bridges_scripts/hydr_openMP_4_threads.sh
+++ bridges_scripts/hydr_openMP_4_threads.sh
@@ -8,10 +8,10 @@
 #SBATCH --partition=RM
 
 module load openmpi/4.1.1-gcc8.3.1
-mkdir hydr
-mkdir hydr/pressure
-mkdir hydr/system
-mkdir hydr/membrane
-mkdir hydr/restarts
+# mkdir hydr
+# mkdir hydr/pressure
+# mkdir hydr/system
+# mkdir hydr/membrane
+# mkdir hydr/restarts
 
-mpirun --map-by ppr:32:node lmp -sf omp -pk omp 4 /jet/home/ajotcham/lammps-29Aug2024/build/lmp -in ../../domain_generation/hydrate.in -var mult 64 -var rand 5  -var FEEDP 1
+mpirun --map-by ppr:32:node /jet/home/ajotcham/lammps-29Aug2024/build/lmp -sf omp -pk omp 4 -in ../../domain_generation/hydrate.in -var mult 64 -var rand 5  -var FEEDP 1
