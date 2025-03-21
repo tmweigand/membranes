7695864a5f4bb306bb243809fa3e0b1e7d058e74
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-03-21 11:33:17 -0400

    updates oligomer

--- rdf/oligmer_examply.py
+++ rdf/oligmer_examply.py
@@ -15,38 +15,39 @@ def oligomer():
 
     _atom_ids = data[:, 0]  # First column
 
-    coordinates = data[:, 1:].astype(float)  # Convert the rest to float
+    spheres = data[:, 1:].astype(float)  # Convert the rest to float
+    # radii = data[:, 4].astype(float)
 
     box = ((-10, 30), (-10, 30), (10, 30))
 
     sd = pmmoto.initialize(voxels=(500, 500, 500), box=box)
 
-    count = 0
-    atom_radii = {}
-    for _id in _atom_ids:
-        if _id[0] == "H":
-            atom_radii[count] = 1.5
-        else:
-            atom_radii[count] = 2.75
-        count += 1
+    # count = 0
+    # atom_radii = {}
+    # for _id in _atom_ids:
+    #     if _id[0] == "H":
+    #         atom_radii[count] = 1.5
+    #     else:
+    #         atom_radii[count] = 2.75
+    #     count += 1
 
-    count = 0
-    atom_ids = {}
-    for atom in _atom_ids:
-        if atom not in atom_ids:
-            atom_ids[count] = count
-            count += 1
+    # count = 0
+    # atom_ids = {}
+    # for atom in _atom_ids:
+    #     if atom not in atom_ids:
+    #         atom_ids[count] = count
+    #         count += 1
 
-    # atoms = pmmoto.domain_generation.particles.initialize_atoms(
-    #     sd, coordinates, atom_radii, atom_ids, by_type=False
-    # )
+    pm = pmmoto.domain_generation.gen_pm_spheres_domain(sd, spheres)
 
-    pm = pmmoto.domain_generation.gen_pm_atom_domain(
-        sd, coordinates, atom_radii, atom_ids
-    )
+    pores = pmmoto.filters.morphological_operators.erode(sd, pm.img, 2.8)
+    pores2 = pmmoto.filters.morphological_operators.dilate(sd, pores, 2.8)
 
     pmmoto.io.output.save_img_data_parallel(
-        "data_out/oligomer_equilibrium_relaxed", sd, pm.img
+        "data_out/oligomer_van_der_waals",
+        sd,
+        pm.img,
+        additional_img={"erode": pores, "dilate": pores2},
     )
 
 
