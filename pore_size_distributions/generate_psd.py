ec243837a991df210f063ab5a24027f3b21ae240
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-04-04 10:26:58 -0400

    refactor generate_psd

--- pore_size_distributions/generate_psd.py
+++ pore_size_distributions/generate_psd.py
@@ -23,12 +23,12 @@ def initialize_domain():
     ]
 
     sd = pmmoto.initialize(
-        voxels=(300, 300, 300),
+        voxels=(500, 500, 500),
         box=box,
         rank=rank,
         subdomains=(2, 2, 2),
         boundary_types=((2, 2), (2, 2), (2, 2)),
-        verlet_domains=(20, 20, 20),
+        verlet_domains=(30, 30, 30),
     )
 
     return sd
@@ -53,48 +53,63 @@ def generate_psd(bridges):
 
     sd = initialize_domain()
 
-    membrane_files = glob.glob("data/membrane_data/*")
-    membrane_file = membrane_files[0]
-
-    membrane_positions, membrane_atom_type, _, _ = (
-        pmmoto.io.data_read.read_lammps_atoms(membrane_file)
-    )
-
-    unique_atom_types = np.unique(membrane_atom_type)
     mem_radii = {}
-    for type in unique_atom_types:
-        element = atom_type_element_map[type]
-        mem_radii[type] = list(
+    for atom_label, element in atom_type_element_map.items():
+        mem_radii[atom_label] = list(
             pmmoto.particles.uff_radius(atom_names=element).values()
         )[0]
 
-    # membrane = pmmoto.particles.initialize_atoms(
+    pore_size_radii = np.linspace(1, 10, 50)
+
+    psd_counts = {}
+    for radius in pore_size_radii:
+        psd_counts[radius] = 0
+
+    membrane_files = glob.glob("data/membrane_data/*")
+
+    for membrane_file in [membrane_files[0]]:
+
+        membrane_positions, membrane_atom_type, _, _ = (
+            pmmoto.io.data_read.read_lammps_atoms(membrane_file)
+        )
+
+        if sd.rank == 0:
+            print("File Read")
+
+        pm = pmmoto.domain_generation.gen_pm_atom_domain(
+            sd, membrane_positions, mem_radii, membrane_atom_type
+        )
+
+        if sd.rank == 0:
+            print("Domain Generated")
+
+        psd = pmmoto.filters.porosimetry.pore_size_distribution(
+            sd, pm, pore_size_radii, inlet=False
+        )
+
+        counts = pmmoto.core.utils.bin_image(sd, psd)
+
+        for radius, _count in counts.items():
+            if radius == 0:
+                continue
+            psd_counts[radius] += _count
+
+        if sd.rank == 0:
+            print(psd_counts)
+
+    if sd.rank == 0:
+        pmmoto.filters.porosimetry.plot_pore_size_distribution(
+            "data_out/psd/", psd_counts
+        )
+
+    # pmmoto.io.output.save_img_data_parallel(
+    #     "data_out/psd_images_small",
     #     sd,
-    #     membrane_positions,
-    #     mem_radii,
-    #     membrane_atom_type,
-    #     by_type=False,
-    #     add_periodic=True,
-    #     set_own=False,
+    #     pm.img,
+    #     additional_img={"psd": psd},
     # )
 
-    # atom_locations = membrane.return_coordinates()
-    # atom_types =
 
-    pm = pmmoto.domain_generation.gen_pm_atom_domain(
-        sd, membrane_positions, mem_radii, membrane_atom_type
-    )
-
-    img = pmmoto.filters.porosimetry.pore_size_distribution(
-        sd, pm, 0.5866666666666667, inlet=False, plot="pdf"
-    )
-
-    pmmoto.io.output.save_img_data_parallel(
-        "data_out/psd_images",
-        sd,
-        pm.img,
-        additional_img={"psd": img},
-    )
-
-
-generate_psd(None)
+if __name__ == "__main__":
+    bridges_run = False
+    generate_psd(bridges_run)
