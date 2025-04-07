ab07508b4b08a687276b027ec8bee4c54c5a8e18
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-04-07 12:52:13 -0400

    adds bridges results and cleans up plotting

--- density_calculations/plot_densities.py
+++ density_calculations/plot_densities.py
@@ -16,10 +16,13 @@ def generate_plots():
         [-288, 238],  # Ignore water reservoirs
     ]
 
-    n_files = 4
-
-    membrane_file = glob.glob("data_out/membrane_bins/bin_membrane.txt")
-    water_file = glob.glob("data_out/water_bins/bin_water.txt")
+    n_files = 13947  # From files processed - see /ocean/projects/cts200024p/ajotcham/membranes/mass_output.out
+    membrane_file = glob.glob(
+        "density_calculations/bridges_results/membrane_bins/bin_membrane.txt"
+    )
+    water_file = glob.glob(
+        "density_calculations/bridges_results/water_bins/bin_water.txt"
+    )
 
     membrane_bins, membrane_density = np.genfromtxt(membrane_file[0], unpack=True)
     water_bins, water_density = np.genfromtxt(water_file[0], unpack=True)
@@ -35,10 +38,37 @@ def generate_plots():
     membrane_density = membrane_density / avogadro / n_files
     water_density = water_density / avogadro / n_files
 
-    plt.plot(membrane_bins, membrane_density / bin_volume)
-    plt.plot(water_bins, water_density / bin_volume)
-    plt.plot(membrane_bins, membrane_density / bin_volume + water_density / bin_volume)
-    plt.show()
+    fig, ax = plt.subplots()
+    fig.set_size_inches(10, 4)
+
+    ax.plot(
+        membrane_bins,
+        membrane_density / bin_volume + water_density / bin_volume,
+        "k",
+        label="Total",
+    )
+    ax.plot(membrane_bins, membrane_density / bin_volume, color="0.5", label="Membrane")
+    ax.plot(water_bins, water_density / bin_volume, "blue", label="Water")
+
+    ax.set_xlabel("z-coordinate (Å)")
+    ax.set_ylabel("Density (g/cm$^3$)")
+
+    ax.set_xlim(left=-200, right=150)
+    # ax.set_ylim(top=1.3)
+
+    # Grid, legend
+    ax.grid(True, linestyle=":", linewidth=1)
+    ax.legend()
+
+    # Add vertical lines
+    ax.axvline(x=-100, color="gold", linestyle="dashed", linewidth=2)
+    ax.axvline(x=100, color="gold", linestyle="dashed", linewidth=2)
+
+    ax.axvline(x=-35, color="salmon", linestyle="dashed", linewidth=2)
+    ax.axvline(x=65, color="salmon", linestyle="dashed", linewidth=2)
+
+    # Save the figure
+    plt.savefig("density_calculations/membrane_density.png")
 
 
 if __name__ == "__main__":
